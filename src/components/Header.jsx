baf896da8917566a379738c14a8a0babcd34965c
lucas789johnson <lucas789johnson@gmail.com>
2025-04-29 11:00:51 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <section className="h-[65vh] md:h-screen flex flex-col justify-center md:justify-end md:pb-[35vh] items-center header relative">
       <div className="relative z-10">
       <h1 className="text-3xl lg:text-8xl font-semibold text-white text-center mb-10">
-        Vote Your Favourite <br /> Ambassaor
+        Vote Your Favourite <br /> Ambassador
       </h1>
       <PopupInfo value={"Vote Favourite Ambassador"} style={true}/>
       </div>
